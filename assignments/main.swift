0c8b08d4d76c3031231c8a57498e50c1805cc9f9
KutayPolat <polatt.kutay@gmail.com>
2022-08-18 20:10:27 -0700

    :fire: finished assignment1

--- assignments/main.swift
+++ assignments/main.swift
@@ -1,20 +1,20 @@
 import Foundation
 
-print("Please enter X1")
-let input1 = readLine()
 
-    print("Please enter X2")
-    let input2 = readLine()
+let question1 = Qestion1()
+question1.solution1()
 
-   print("Please enter X3")
-   let input3 = readLine()
+let question2 = Question2()
+question2.solution2()
 
-   print("Please enter X4")
-   let input4 = readLine()
+let question3 = Question3()
+question3.solution()
 
-   if let stringX1 = input1, let stringX2 = input2, let stringX3 = input3, let stringX4 = input4 {
-       if let x1 = Double(stringX1), let x2 = Double(stringX2), let x3 = Double(stringX3), let x4 = Double(stringX4) {
-           print("Result: \(max(x1, x2, x3, x4) - min(x1, x2, x3, x4) + pow(x1, x2) + abs(x3 - x4))")
-       }
-   }
+let question4 = Question4()
+question4.solution4()
+
+let question5 = Question5()
+question5.solution5()
+
+
 
